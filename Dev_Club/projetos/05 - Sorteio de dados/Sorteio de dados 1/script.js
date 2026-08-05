3305cd342b8ec59c711dd769e35c1a8355bd97dc
danieldevbz39 <adsdaniel85@gmail.com>
2026-08-05 17:21:16 -0300

    min max

--- Dev_Club/projetos/05 - Sorteio de dados/Sorteio de dados 1/script.js
+++ Dev_Club/projetos/05 - Sorteio de dados/Sorteio de dados 1/script.js
@@ -4,6 +4,12 @@ function generateNumber(){
     const max = Math.ceil(document.querySelector('.input-max').value)
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
     
-
+    if (max > min) {
+        const result = Math.floor(Math.random() * (max - min +1)) + min
     alert(result)
+    } else {
+        alert('O valor minimo tem que ser menor que o valor máximo.')
+    }
+
+    
 }
