ad1bc4a3fbb0e07711d8920966cb0ea1dd75fe38
danieldevbz39 <adsdaniel85@gmail.com>
2026-08-08 20:26:23 -0300

    dados

--- Dev_Club/projetos/06 - Contatos - Cadastros/2 - Buscador de contatos/script.js
+++ Dev_Club/projetos/06 - Contatos - Cadastros/2 - Buscador de contatos/script.js
@@ -1,1 +1,30 @@
-console.log('oi')
+
+let p = document.querySelector("p")
+let input = document.querySelector("input")
+
+const contacts = [
+  { name: 'Rodolfo', number: '(12) 9999-9999' },
+  { name: "Gabriel", number: "(12) 9999-9999" },
+  { name: "Phelipe", number: "(12) 9999-9999" },
+  { name: "Paulo", number: "(12) 9999-9999" },
+  { name: "Aline", number: "(12) 9999-9999" },
+  { name: "Maria", number: "(12) 9999-9999" },
+  { name: "Daniel", number: "(12) 9999-9999" },
+  { name: "Ana", number: "(12) 9999-9999" },
+  { name: "Sará", number: "(12) 9999-9999" },
+  { name: "Antônio", number: "(12) 9999-9999" },
+
+]
+
+function search(){
+
+  for (let i = 0; i < contacts.length; i++){
+    if(input.value.toLocaleLowerCase() === contacts [i].name.toLocaleLowerCase()){
+      p.innerHTML = `Contato Encontrado. Nome: ${contacts[i].name} Tel: ${contacts[i].number}`
+      
+      break
+    } else {
+      p.innerHTML = "Contato não encontrado, tente novamente."
+    }
+  }
+}
--- Dev_Club/projetos/06 - Contatos - Cadastros/3 - Contatos/script.js
+++ Dev_Club/projetos/06 - Contatos - Cadastros/3 - Contatos/script.js
@@ -1,1 +1,16 @@
-console.log('oi')
+
+    let p = document.querySelector('p')
+    let input = document.querySelector('input')
+
+    const contacts = [
+        {name: 'Rodolfo', number: '(19) 9999-5555'},
+        {name: 'Paulo', number: '(31) 9987-5355'},
+        {name: 'Aline', number: '(22) 9965-5555'},
+        {name: 'Maria', number: '(15) 99129-2255'},
+        {name: 'Daniel', number: '(11) 9933-5555'},
+        {name: 'Ana', number: '(10) 9995-5455'},
+    ]
+
+    function search(){
+        console.log(input.value)
+    }
