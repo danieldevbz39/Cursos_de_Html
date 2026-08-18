4399c47071a9cb8c7786fd58155d09f28034f352
Daniel Siqueira <149271743+danieldevbz39@users.noreply.github.com>
2026-08-18 19:45:35 -0300

    calculadora
    feito em javascript

---
+++ Dev_Club/projetos/07 - Calculadora/script.js
@@ -0,0 +1,29 @@
+
+
+function sum (value, value2){
+    console.log (value + value2)
+}
+
+sum (15, 15)
+
+
+
+function sub (value, value2){
+    console.log (value - value2)
+}
+
+sub (25, 15)
+
+
+function mult (value, value2){
+    console.log (value * value2)
+}
+
+mult (10, 10)
+
+
+function div (value, value2){
+    console.log (value / value2)
+}
+
+div (15, 2)
